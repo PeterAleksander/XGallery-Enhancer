a9cb48d7c40ebc7b9b89c3390f433236909e24db
PeterAleksander <PeterAleksanderSlothAndersen@gmail.com>
2024-02-23 23:23:05 -0600

    Removed Header and Side Bar, main content not 100VW yet

--- content.js
+++ content.js
@@ -15,3 +15,35 @@ chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
     checkURL();
   }
 });
+
+// Function to hide specific elements
+function hideElements() {
+  // Hide the header
+  const headerElement = document.querySelector('header[role="banner"]');
+  if (headerElement) {
+    headerElement.style.display = "none"; // Hide the element
+    console.log("Header element hidden");
+  }
+
+  // Hide the sidebar column
+  const sidebarElement = document.querySelector(
+    'div[data-testid="sidebarColumn"]'
+  );
+  if (sidebarElement) {
+    sidebarElement.style.display = "none";
+    console.log("Sidebar column hidden");
+  }
+}
+
+// Create an observer instance linked to a callback function
+const observer = new MutationObserver(function (mutations, me) {
+  // Check if the target elements are now available
+  hideElements(); // Call hideElements to hide both the header and sidebar
+});
+
+// Start observing the document body for added nodes
+observer.observe(document.body, {
+  childList: true, // Observe direct children
+  subtree: true, // and lower descendants
+  attributes: false,
+});
